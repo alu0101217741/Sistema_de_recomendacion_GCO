33fdd6b4c9c075d0b01722073c3327159d81992b
Alberto Mendoza Rodríguez <alu0101217741@ull.edu.es>
2021-11-28 21:22:33 +0000

    Add files via upload

---
+++ docs/src/formulario.js
@@ -0,0 +1,168 @@
+/**
+ * Evento que se ejecuta cuando el usuario hace click sobre el botón del formulario.
+ */
+document.getElementById('read_button').addEventListener('click', function() {
+    
+    /**
+     * Se avisa al usuario mediante un alert en caso de que no haya introducido ningín formulario.
+     */
+    if(document.getElementById("file-input").files.length == 0) {
+        alert('ERROR: No ha introducido ningún fichero.');
+		return;
+	}
+
+    /**
+     * Se obtienen los datos introducidos por el usuario en el formulario.
+     */
+    const metrica = document.getElementById("metrica").value;
+    const num_vecinos = document.getElementById("num_vecinos").value;
+    const tipo_prediccion = document.getElementById("prediccion").value;
+
+    /**
+     * Se obtiene el fichero introducido por el usuario.
+     */
+	let file = document.getElementById("file-input").files[0];
+
+    /**
+     * Se crea un objeto FileReader para poder leer el contenido del fichero.
+     */
+	let reader = new FileReader();
+
+    /**
+     * Evento que se ejecuta cuando ha finalizado correctamente la lectura del fichero.
+     */
+	reader.addEventListener('load', function(e) {
+
+        // Se obtienen los datos leídos del fichero.
+        let text = String(e.target.result);
+         
+        // Se convierte el contenido del fichero en una matriz.
+        let matrix = [];
+        let array_aux = [];
+         
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] != ' ' && text[i] != '\r' && text[i] != '\n') {
+                if (text[i] == '-') {
+                    array_aux.push(-1);
+                } else {
+                    array_aux.push(Number(text[i]));
+                }                
+            }
+            if (text[i] == '\n' || i == text.length - 1) {
+                matrix.push(array_aux);
+                array_aux = [];
+            }
+        }
+
+        // Se crea un objeto de la clase Recomendador.
+        const recomendador = new Recomendador(matrix, metrica, num_vecinos, tipo_prediccion);
+
+        // Se llaman a los métodos de la clase Recomendador para obtener los resultados y se muestran empleando código HTML.
+        document.getElementById("matriz_utilidad").innerHTML = '<h5>Matriz de utilidad original</h5>' + mostrar_matriz(recomendador.get_matriz_utilidad());
+        document.getElementById("matriz_similaridad").innerHTML = '<h5>Matriz de similitud</h5>' + mostrar_matriz(recomendador.get_matriz_similitud(), "Usuario");
+        document.getElementById("matriz_resuelta").innerHTML = '<h5>Matriz de utilidad con predicciones resueltas</h5>' + mostrar_matriz(recomendador.recomendar(), "Item", true, recomendador.get_matriz_utilidad());
+        document.getElementById("vecinos_usuarios").innerHTML = '<div class="center"><h5 class="center">Vecinos seleccionados</h5></div>' + mostrar_vecinos(recomendador.get_vecinos_usuarios());
+        document.getElementById("calculo_prediccion").innerHTML = '<div class="center"><h5 class="center">Cálculo de cada predicción</h5></div>' + mostrar_calculo(recomendador.get_calculo_predicciones(), recomendador.get_tipo_prediccion());
+     });
+
+    /**
+     * Evento que se ejecuta si ocurre algún error en la lectura del fichero.
+     */
+	reader.addEventListener('error', function() {
+	    alert('ERROR: Fallo al leer el contenido del fichero.');
+	});
+
+    /**
+     * Se lee como un fichero de texto el fichero introducido por el usuario.
+     */
+	reader.readAsText(file);
+});
+
+/**
+ * Función para mostrar una matriz, en caso de que se le pasen la matriz de utilidad o la matriz resultado del cálculo, se marca en color azul las predicciones.
+ * @param matriz Matriz que se va a mostrar.
+ * @param elemento Indica que si en la cabecera de la matriz se debe mostrar "Item" o "Usuario", por defecto es "Item".
+ * @param solucion  Indica si se va a mostrar la matriz con las predicciones resueltas, esto se utiliza para destacar en color azul 
+ *                  las casillas con las predicciones obtenidas.
+ * @param matriz_utilidad Esta matriz se pasa a la función sólo cuando se va a mostrar la matriz con las predicciones resueltas para 
+ *                        saber donde se encuentran los "-" y marcar las soluciones.
+ * @returns Tabla en código HTML.
+ */
+function mostrar_matriz(matriz, elemento = "Item", solucion = false, matriz_utilidad =[]) {
+    let tabla = '<div class="col s12" id="table-scroll"><table class="stripped"><thead><tr><th> </th>';
+
+    for (let i = 0; i < matriz[0].length; i++) {
+        tabla += '<th> ' + elemento + " " + (i + 1) + '</th>';
+    }
+    tabla += '</tr></thead><tbody>';
+
+    for (let i = 0; i < matriz.length; i++) {
+        tabla += '<tr><th> Usuario ' + (i + 1) + '</th>';
+        for (let j = 0; j < matriz[i].length; j++) {
+            if (solucion && matriz_utilidad[i][j] == -1) {
+                tabla += '<td id="mostrar_prediccion" class=" cyan darken-2 white-text">' + Math.round((matriz[i][j] + Number.EPSILON) * 100) / 100 + '</td>';
+            } else if (matriz[i][j] == -1) {
+                tabla += '<td id="mostrar_prediccion" class=" cyan darken-2 white-text"> - </td>';
+            } else {
+                tabla += '<td>' + Math.round((matriz[i][j] + Number.EPSILON) * 100) / 100 + '</td>';
+            }
+        }
+        tabla += '</tr>';
+    }
+    tabla += '</tbody></table></div>';
+    return tabla;
+}
+
+/**
+ * Función para mostrar los vecinos seleccionados para un determinado usuario e item.
+ * @param vecinos_usuarios Matriz donde cada fila tiene el formato [usuario, item, vecinos], donde vecinos es un vector.
+ * @returns Lista en código HTML con los vecinos de cada usuario e item.
+ */
+function mostrar_vecinos(vecinos_usuarios) {
+    let resultado = '<div class="col s8 offset-s2"><ul class="collection">';
+    for (let i = 0; i < vecinos_usuarios.length; i++) {
+        resultado += '<li class="collection-item">Para predecir la valoración del <b>Usuario ' + (vecinos_usuarios[i][0] + 1) + '</b> ' +
+                        'para el <b>item ' + (vecinos_usuarios[i][1] + 1) + '</b> se han seleccionado los <b>vecinos: ';
+        for(let j = 0; j < vecinos_usuarios[i][2].length; j++) {
+            resultado +=  vecinos_usuarios[i][2][j].vecino + 1;
+            if (j != vecinos_usuarios[i][2].length - 1)
+                resultado += ', ';
+        }
+        resultado += '</b>.</li>';
+    }
+    resultado += '</ul></div>';
+    return resultado;
+}
+
+/**
+ * Función para mostrar los cálculos realizados en cada predicción.
+ * @param predicciones Matriz con formato [usuario, item, sumatorio1, sumatorio2, prediccion] en caso de predicción simple,
+ *                     y [usuario, item, media_usuario, sumatorio1, sumatorio2, prediccion] para la diferencia con la media.
+ * @param tipo_prediccion Indica si se ha utilizado predicción simple o diferencia con la media.
+ * @returns Lista en código HTML con el cálculo de cada prediccón.
+ */
+function mostrar_calculo(predicciones, tipo_prediccion) {
+    let calculos = '<div class="col s8 offset-s2"><ul class="collection">';
+    for (let i = 0; i < predicciones.length; i++) {
+        calculos += '<li class="collection-item">Cálculo para el <b>Usuario ' + (predicciones[i][0] + 1) + '</b> con el <b>item ' +
+        (predicciones[i][1] + 1) + '</b>:  &nbsp <b>';
+        if (tipo_prediccion == "Predicción simple") {
+            calculos += redondear(predicciones[i][2]) + ' / ' + redondear(predicciones[i][3]) + ' = ' + redondear(predicciones[i][4]);
+        } else {
+            calculos += redondear(predicciones[i][2]) + ' + (' + redondear(predicciones[i][3]) + ' / ' + redondear(predicciones[i][4]) + 
+                    ') = ' + redondear(predicciones[i][5]);
+        }
+        calculos += '</b></li>';
+    }
+    calculos += '</ul></div>';
+    return calculos;
+}
+
+/**
+ * Función utilizada para redondear los cálculos de la función mostrar_calculo() y así hacer más legible el código.
+ * @param numero_decimal Número que se va a redondear.
+ * @returns Número redondeado.
+ */
+function redondear(numero_decimal) {
+    return Math.round((numero_decimal + Number.EPSILON) * 100) / 100;
+}
---
+++ docs/src/recomendador.js
@@ -0,0 +1,270 @@
+/**
+ * Clase que implementa un recomendador siguiendo el método de filtrado colaborativo.
+ */
+class Recomendador {
+    /**
+     * Constructor de la clase.
+     * @param matriz Matriz de utilidad original.
+     * @param metrica El tipo de métrica.
+     * @param num_vecinos La cantidad de vecinos.
+     * @param tipo_prediccion El tipo de predcción a utilizar.
+     */
+    constructor(matriz, metrica, num_vecinos, tipo_prediccion) {
+        this.matriz_utilidad = matriz;
+        this.metrica = metrica;
+        this.num_vecinos = num_vecinos;
+        this.tipo_prediccion = tipo_prediccion;
+        this.matriz_similitud = this.crear_matriz_similitud(metrica);
+        this.vecinos_usuarios = [];
+        this.calculo_predicciones = [];
+    }
+
+    /**
+     * Método para calcular la matriz de similitud.
+     * @param metrica El tipo de métrica ha utilizar.
+     * @returns La matriz de similitud.
+     */
+    crear_matriz_similitud(metrica) {
+        let matriz_similitud = [];
+        let similitud_vecinos = [];
+        for (let i = 0; i < this.matriz_utilidad.length; i++) {
+           for (let j = 0; j < this.matriz_utilidad.length; j++) {
+                switch (metrica) {
+                    case 'Correlación de Pearson':
+                        similitud_vecinos.push(this.correlacion_pearson(i, j));
+                        break;
+                    case 'Distancia coseno':
+                        similitud_vecinos.push(this.distancia_coseno(i, j));
+                        break;
+                    case 'Distancia Euclídea':
+                        similitud_vecinos.push(this.distancia_euclidea(i, j));
+                        break;
+                }
+           }
+           matriz_similitud.push(similitud_vecinos);
+           similitud_vecinos = [];
+        }
+        return matriz_similitud;
+    }
+
+    /**
+     * Método para calcular la similitud según la correlación de Pearson.
+     * @param usuario_u Usuario u utilizado para el cálculo.
+     * @param usuario_v Usuario v utilizado para el cálculo.
+     * @returns La similitud según la correlación de Pearson entre los usuarios u y v.
+     */
+    correlacion_pearson(usuario_u, usuario_v) {
+        let media_u = this.calcular_media(usuario_u);
+        let media_v = this.calcular_media(usuario_v);
+        let sumatorio1 = 0;
+        let sumatorio2 = 0;
+        let sumatorio3 = 0;
+
+        for (let i = 0; i < this.matriz_utilidad[usuario_u].length; i++) {
+            if (this.matriz_utilidad[usuario_u][i] != -1 && this.matriz_utilidad[usuario_v][i] != -1) {
+                sumatorio1 += (this.matriz_utilidad[usuario_u][i] - media_u) * (this.matriz_utilidad[usuario_v][i] - media_v);
+                sumatorio2 += Math.pow(this.matriz_utilidad[usuario_u][i] - media_u, 2);
+                sumatorio3 += Math.pow(this.matriz_utilidad[usuario_v][i] - media_v, 2);
+            }
+        }
+        let similitud = sumatorio1 / (Math.sqrt(sumatorio2) * Math.sqrt(sumatorio3));
+        if (similitud < 0) {
+            similitud = (similitud - (-1)) / (1 - (-1));
+        }
+        return similitud;
+    }
+
+    /**
+     * Método para calcular la similitud según la distancia coseno.
+     * @param usuario_u Usuario u utilizado para el cálculo.
+     * @param usuario_v Usuario v utilizado para el cálculo.
+     * @returns La similitud según la distancia coseno entre los usuarios u y v.
+     */
+    distancia_coseno(usuario_u, usuario_v) {
+        let sumatorio1 = 0;
+        let sumatorio2 = 0;
+        let sumatorio3 = 0;
+        for (let i = 0; i < this.matriz_utilidad[usuario_u].length; i++) {
+            if (this.matriz_utilidad[usuario_u][i] != -1 && this.matriz_utilidad[usuario_v][i] != -1) {
+                sumatorio1 += this.matriz_utilidad[usuario_u][i] * this.matriz_utilidad[usuario_v][i];
+                sumatorio2 += Math.pow(this.matriz_utilidad[usuario_u][i], 2);
+                sumatorio3 += Math.pow(this.matriz_utilidad[usuario_v][i], 2);
+            }
+        }
+        return sumatorio1 / (Math.sqrt(sumatorio2) * Math.sqrt(sumatorio3));
+    }
+
+    /**
+     * Método para calcular la similitud según la distancia Euclídea.
+     * @param usuario_u Usuario u utilizado para el cálculo.
+     * @param usuario_v Usuario v utilizado para el cálculo.
+     * @returns La similitud según la distancia Euclídea entre los usuarios u y v.
+     */
+    distancia_euclidea(usuario_u, usuario_v) {
+        let sumatorio = 0;
+        for (let i = 0; i < this.matriz_utilidad[usuario_u].length; i++) {
+            if (this.matriz_utilidad[usuario_u][i] != -1 && this.matriz_utilidad[usuario_v][i] != -1) 
+                sumatorio += Math.pow(this.matriz_utilidad[usuario_u][i] - this.matriz_utilidad[usuario_v][i], 2);
+        }
+        return Math.sqrt(sumatorio);
+    }
+
+    /**
+     * Método para calcular la media de las valoraciones de un usuario.
+     * @param usuario Usuario que se va a utilizar para calcular la media de sus valoraciones.
+     * @returns La media de las valoraciones.
+     */
+    calcular_media(usuario) {
+        let media = 0;
+        let total = 0;
+        for (let i = 0; i < this.matriz_utilidad[usuario].length; i++) {
+            if (this.matriz_utilidad[usuario][i] != -1) {
+                media += this.matriz_utilidad[usuario][i];
+                total++;
+            }
+        }
+        return media / total;
+    }
+
+    /**
+     * Método para calcular los vecinos de un usuario para un determinado item.
+     * @param usuario El usuario al que se le van a calcular los vecinos.
+     * @param item El item que no ha valorado el usuario.
+     * @returns Los vecinos de un usuario para un determinado item.
+     */
+    calcular_vecinos(usuario, item) {
+        let similitud_vecinos = [];
+        let seleccion_vecinos = [];
+        for (let i = 0; i < this.matriz_utilidad.length; i++) {
+            if (i != usuario)
+                similitud_vecinos.push({vecino: i, similitud: this.matriz_similitud[usuario][i]});
+        }
+        if (this.metrica == "Correlación de Pearson" || this.metrica == "Distancia coseno") {
+            similitud_vecinos.sort(function(a, b) {
+                return b.similitud - a.similitud;
+            });
+        } else {
+            similitud_vecinos.sort(function(a, b) {
+                return a.similitud - b.similitud;
+            });
+        }
+        let i = 0;
+        while ((seleccion_vecinos.length < this.num_vecinos) && (i < similitud_vecinos.length)) {
+            if (this.matriz_utilidad[similitud_vecinos[i].vecino][item] != -1)
+                seleccion_vecinos.push(similitud_vecinos[i]);
+            i++;
+        }
+        return seleccion_vecinos;
+    }
+
+    /**
+     * Método para calcular la predicción simple.
+     * @param usuario El usuario al que se le va a calcular la predicción.
+     * @param item El item que no ha valorado el usuario.
+     * @returns La predicción calculada.
+     */
+    prediccion_simple(usuario, item) {
+        let sumatorio1 = 0;
+        let sumatorio2 = 0;
+        let vecinos = this.calcular_vecinos(usuario, item);
+        this.vecinos_usuarios.push([usuario, item, vecinos]);
+        for (let i = 0; i < vecinos.length; i++) {
+            sumatorio1 += vecinos[i].similitud * this.matriz_utilidad[vecinos[i].vecino][item];
+            sumatorio2 += Math.abs(vecinos[i].similitud);
+        }
+        let prediccion = sumatorio1/sumatorio2;
+        this.calculo_predicciones.push([usuario, item, sumatorio1, sumatorio2, prediccion]);
+        return prediccion;
+    }
+
+    /**
+     * Método para calcular la predicción utilizando la diferencia con la media.
+     * @param usuario El usuario al que se le va a calcular la predicción.
+     * @param item El item que no ha valorado el usuario.
+     * @returns La predicción calculada.
+     */
+    diferencia_media(usuario, item) {
+        let sumatorio1 = 0;
+        let sumatorio2 = 0;
+        let vecinos = this.calcular_vecinos(usuario, item);
+        this.vecinos_usuarios.push([usuario, item, vecinos]);
+        let media_usuario = this.calcular_media(usuario);
+        for (let i = 0; i < vecinos.length ; i++) {
+            sumatorio1 += vecinos[i].similitud * (this.matriz_utilidad[vecinos[i].vecino][item] - this.calcular_media(vecinos[i].vecino));
+            sumatorio2 += Math.abs(vecinos[i].similitud);
+        }
+        let prediccion = media_usuario + (sumatorio1 / sumatorio2);
+        this.calculo_predicciones.push([usuario, item, media_usuario, sumatorio1, sumatorio2, prediccion]);
+        return prediccion;
+    }
+
+    /**
+     * Método que recorre la matriz de utilidad original buscando los items no valorados y calculando sus predicciones.
+     * @returns La matriz de utilidad con las predicciones resueltas.
+     */
+    recomendar() {
+        let matriz_resuelta = [];
+        for (let i = 0; i < this.matriz_utilidad.length; i++) {
+            matriz_resuelta.push([]);
+            for (let j = 0; j < this.matriz_utilidad[i].length; j++) {
+                matriz_resuelta[i][j] = this.matriz_utilidad[i][j];
+            }
+        }
+
+        for (let i = 0; i < this.matriz_utilidad.length; i++) {
+            for (let j = 0; j < this.matriz_utilidad[i].length; j++) {
+                if (this.matriz_utilidad[i][j] == -1) {
+                    switch (this.tipo_prediccion) {
+                        case 'Predicción simple':
+                            matriz_resuelta[i][j] = this.prediccion_simple(i, j);
+                            break;
+                        case 'Diferencia con la media':
+                            matriz_resuelta[i][j] = this.diferencia_media(i, j);
+                            break;
+                    }
+                }
+            }
+        }
+        return matriz_resuelta;
+    }
+
+    /**
+     * Método que devuelve la matriz de utilidad original.
+     * @returns Matriz de utilidad original.
+     */
+    get_matriz_utilidad() {
+        return this.matriz_utilidad;
+    }
+
+    /**
+     * Método que devuelve el tipo de predicción utilizada.
+     * @returns Tipo de predicción.
+     */
+    get_tipo_prediccion() {
+        return this.tipo_prediccion;
+    }
+
+    /**
+     * Método que devuelve la matriz de similitud que se ha calculado.
+     * @returns Matriz de similitud.
+     */
+    get_matriz_similitud() {
+        return this.matriz_similitud;
+    }
+
+    /**
+     * Método que devuelve los vecinos para cada uno de los usuarios e items.
+     * @returns Vecinos para cada para cada usuario según el item.
+     */
+    get_vecinos_usuarios() {
+        return this.vecinos_usuarios;
+    }
+
+    /**
+     * Método que devuelve los valores utilizados en el cálculo de cada predicción.
+     * @returns Los cálculos realizados en las predicciones.
+     */
+    get_calculo_predicciones() {
+        return this.calculo_predicciones;
+    }
+}
